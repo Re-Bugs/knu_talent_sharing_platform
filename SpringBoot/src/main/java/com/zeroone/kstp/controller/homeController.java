e9486eb8d83cd65d6752246aa9a00f47f737330b
SeungWook_Lee <leesw_0924@icloud.com>
2024-11-14 23:18:42 +0900

    html 기본 뼈대 완성

--- SpringBoot/src/main/java/com/zeroone/kstp/controller/homeController.java
+++ SpringBoot/src/main/java/com/zeroone/kstp/controller/homeController.java
@@ -1,6 +1,7 @@
 package com.zeroone.kstp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,6 +9,30 @@ public class homeController {
 
     @GetMapping("/")
     public String home(){
-        return "main";
+        return "home";
+    }
+
+    @GetMapping("/learning_core")
+    public String learningCore(Model model){
+        model.addAttribute("currentMenu", "description");
+        return "learning_core/learning_core_main";
+    }
+
+    @GetMapping("/major_learner")
+    public String majorLearner(Model model){
+        model.addAttribute("currentMenu", "description");
+        return "major_learner/major_learner_main";
+    }
+
+    @GetMapping("/challenge_learner")
+    public String challengeLearner(Model model){
+        model.addAttribute("currentMenu", "description");
+        return "challenge_learner/challenge_learner_main";
+    }
+
+    @GetMapping("/introduction")
+    public String introduction(Model model){
+        model.addAttribute("currentMenu", "introduction");
+        return "community/introduction";
     }
 }
